eac9a6a9ea770a430c61676bf85b7b07df6db1a8
vishnu <vishnuvijayakumar125@gmail.com>
2020-06-18 15:34:57 +1000

    Updates

---
+++ precache-manifest.403fd66ac18eff277201b68930031ee8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0fdd4b91e6c7294ef5607ac78b6beb6",
+    "url": "/yummy-app/index.html"
+  },
+  {
+    "revision": "0a5a99768d0004294c2f",
+    "url": "/yummy-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "15c3f7b9de67a6f1724b",
+    "url": "/yummy-app/static/js/2.ff4556eb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/yummy-app/static/js/2.ff4556eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a5a99768d0004294c2f",
+    "url": "/yummy-app/static/js/main.4e152bca.chunk.js"
+  },
+  {
+    "revision": "78a0bcff5c589ef14906",
+    "url": "/yummy-app/static/js/runtime-main.4bde6286.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/yummy-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
